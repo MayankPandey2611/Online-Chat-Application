c83c129a1dd86b9d11c5952140510c83f262dbd6
MayankPandey2611 <kitupnd@gmail.com>
2026-01-14 21:04:52 +0530

    changes

--- chatweb/urls.py
+++ chatweb/urls.py
@@ -25,3 +25,8 @@ urlpatterns = [
     path('home/', home),
     path('chat/<str:room_name>/', room),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
